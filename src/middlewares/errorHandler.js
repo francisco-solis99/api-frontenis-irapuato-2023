7840d28289f2fcc5909025d4dae2fe104fb32286
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-22 23:38:03 -0600

    chore: :goal_net: Create a error middleware with from Error extends class

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,16 @@
+import AppError from '../errors/AppError.js';
+
 const errorHandler = (err, req, res, next) => {
   // if no status or message in error use default 500 code and message
   const statusCode = err.status ?? 500;
   const message = err.message || 'Something went wrong.';
+
+  if (err instanceof AppError) {
+    const { message, statusCode } = err;
+    return res.status(statusCode).json({
+      message
+    });
+  }
 
   // returns error status code and message
   return res.status(statusCode).json({
