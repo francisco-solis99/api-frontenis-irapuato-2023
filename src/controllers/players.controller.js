ae3ec2013b024fc882a27d410fb7120fa0803706
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-23 00:11:47 -0600

    feat: :sparkles: Add players and teams controllers

--- src/controllers/players.controller.js
+++ src/controllers/players.controller.js
@@ -1,45 +1,35 @@
 import sequelize from '../config/config.js';
-
+import AppError from '../errors/AppError.js';
 const { players: playersModel } = sequelize.models;
 
 // Get all the players
 export async function getPlayers(req, res) {
   const { limit } = req.query;
-  try {
-    const allPlayers = await playersModel.findAndCountAll({
-      limit: limit ? Number(limit) : limit
-    });
-    return res.status(200).json(allPlayers);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+  const allPlayers = await playersModel.findAndCountAll({
+    limit: limit ? Number(limit) : limit
+  });
+  if (!allPlayers) throw new AppError('Players not found', 400);
+  return res.status(200).json(allPlayers);
 }
 
-// Get specific player
+// Get player by id
 export async function getPlayerById(req, res) {
   const { id } = req.params;
-  try {
-    const player = await playersModel.findByPk(id);
-    return res.state(200).json(player);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
+  const player = await playersModel.findByPk(id);
+  if (!player) {
+    throw new AppError('Player not found', 400);
   }
+  return res.state(200).json(player);
 }
 
 // Create a player
 export async function createPlayer(req, res) {
-  const { name, nickName, pictureUrl, city, teamId } = req.body;
-  try {
-    const newPlayer = playersModel.create({
-      name,
-      nickName,
-      pictureUrl,
-      city,
-      teamId
-    });
-    await newPlayer.save();
-    return res.status(201).json({ message: 'Player created successfully', data: newPlayer });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+  const { name, city, teamId } = req.body;
+  const newPlayer = await playersModel.create({
+    name,
+    city,
+    teamId
+  });
+  await newPlayer.save();
+  return res.status(201).json({ message: 'Player created successfully', data: newPlayer });
 }
--- src/controllers/teams.controller.js
+++ src/controllers/teams.controller.js
@@ -1,42 +1,37 @@
 import sequelize from '../config/config.js';
+import AppError from '../errors/AppError.js';
 
 const { teams: teamsModel } = sequelize.models;
 
 // Get all the teams
 export async function getTeams(req, res) {
   const { limit } = req.query;
-  try {
-    const allTeams = await teamsModel.findAndCountAll({
-      limit: limit ? Number(limit) : limit
-    });
-    return res.status(200).json(allTeams);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+  const allTeams = await teamsModel.findAndCountAll({
+    limit: limit ? Number(limit) : limit
+  });
+
+  if (!allTeams) throw new AppError('Teams not found', 400);
+  return res.status(200).json(allTeams);
 }
 
 // Get specific team
 export async function getTeamById(req, res) {
   const { id } = req.params;
-  try {
-    const team = await teamsModel.findByPk(id);
-    return res.state(200).json(team);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+  if (!id) throw AppError('No id as param', 404);
+
+  const team = await teamsModel.findByPk(id);
+
+  if (!team) throw new AppError('Team not found', 400);
+  return res.state(200).json(team);
 }
 
 // Create a Team
 export async function createTeam(req, res) {
   const { wins, losses } = req.body;
-  try {
-    const newTeam = teamsModel.create({
-      wins,
-      losses
-    });
-    await newTeam.save();
-    return res.status(201).json({ message: 'Team created successfully', data: newTeam });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
+  const newTeam = await teamsModel.create({
+    wins,
+    losses
+  });
+  await newTeam.save();
+  return res.status(201).json({ message: 'Team created successfully', data: newTeam });
 }
