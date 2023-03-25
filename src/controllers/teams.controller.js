33699b6de0d9a4fa174ea5db5636146639b46a16
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-25 13:14:34 -0600

    refactor: :recycle: Include the player model when get the teams

--- src/controllers/teams.controller.js
+++ src/controllers/teams.controller.js
@@ -1,13 +1,18 @@
 import sequelize from '../config/db.js';
 import AppError from '../errors/AppError.js';
 
-const { teams: teamsModel } = sequelize.models;
+const { teams: teamsModel, players: playersModel } = sequelize.models;
 
 // Get all the teams
 export async function getTeams(req, res) {
   const { limit } = req.query;
   const allTeams = await teamsModel.findAndCountAll({
-    limit: limit ? Number(limit) : limit
+    limit: limit ? Number(limit) : limit,
+    include: [
+      {
+        model: playersModel, attributes: ['id', 'name', 'city']
+      }
+    ]
   });
 
   if (!allTeams) throw new AppError('Teams not found', 400);
@@ -19,10 +24,17 @@ export async function getTeamById(req, res) {
   const { id } = req.params;
   if (!id) throw AppError('No id as param', 404);
 
-  const team = await teamsModel.findByPk(id);
+  const team = await teamsModel.findOne({
+    where: { id },
+    include: [
+      {
+        model: playersModel, attributes: ['id', 'name', 'city']
+      }
+    ]
+  });
 
   if (!team) throw new AppError('Team not found', 400);
-  return res.state(200).json(team);
+  return res.status(200).json({ message: 'Team found successfully', data: team });
 }
 
 // Create a Team
@@ -38,7 +50,7 @@ export async function createTeam(req, res) {
 }
 
 // Update some team
-export async function updatePlayer(req, res) {
+export async function updateTeam(req, res) {
   const { body, params: { id } } = req;
   const team = await teamsModel.findByPk(id);
   if (!team) return res.status(404).json({ message: 'Team not found', data: null });
