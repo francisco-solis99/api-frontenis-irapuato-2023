6a3b1d9e558bfa5932b2affc7b48d6ebc5cd5860
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-25 13:14:03 -0600

    refactor: :recycle: Include the teams model when get the groups and add getallinfo controller

--- src/controllers/groups.controller.js
+++ src/controllers/groups.controller.js
@@ -1,12 +1,17 @@
 import sequelize from '../config/db.js';
 import AppError from '../errors/AppError.js';
-const { groups: groupsModel } = sequelize.models;
+const { groups: groupsModel, teams: teamsModel, players: playersModel } = sequelize.models;
 
 // Get all the groups
 export async function getGroups(req, res) {
   const { limit } = req.query;
   const allGroups = await groupsModel.findAndCountAll({
-    limit: limit ? Number(limit) : limit
+    limit: limit ? Number(limit) : limit,
+    include: [
+      {
+        model: teamsModel, attributes: ['id', 'wins', 'gamesPlayed', 'groupId']
+      }
+    ]
   });
   if (!allGroups) throw new AppError('Groups not found', 400);
   return res.status(200).json(allGroups);
@@ -15,11 +20,18 @@ export async function getGroups(req, res) {
 // Get group by id
 export async function getGroupById(req, res) {
   const { id } = req.params;
-  const group = await groupsModel.findByPk(id);
+  const group = await groupsModel.findOne({
+    where: { id },
+    include: [
+      {
+        model: teamsModel, attributes: ['id', 'wins', 'gamesPlayed', 'groupId']
+      }
+    ]
+  });
   if (!group) {
     throw new AppError('Group not found', 400);
   }
-  return res.state(200).json(group);
+  return res.status(201).json({ message: 'Group found', data: group });
 }
 
 // Create a match
@@ -42,4 +54,27 @@ export async function updateGroup(req, res) {
     name: body.name
   });
   return res.json({ message: 'Group created successfully', data: updatedGroup });
+}
+
+// Get all info by group id
+export async function getAllGroupInfoById(req, res) {
+  const { id } = req.params;
+  const group = await groupsModel.findOne({
+    where: { id },
+    include: [
+      {
+        model: teamsModel,
+        attributes: ['id', 'wins', 'gamesPlayed', 'groupId'],
+        include: [
+          {
+            model: playersModel
+          }
+        ]
+      }
+    ]
+  });
+  if (!group) {
+    throw new AppError('Group not found', 400);
+  }
+  return res.status(201).json({ message: 'Group found', data: group });
 }
