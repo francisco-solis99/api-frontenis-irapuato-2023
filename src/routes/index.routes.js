1eb2bfc1a8ac6714bc8d39c8fb4684a7c3bd98df
francisco-solis99 <franciscosoliscss@gmail.com>
2023-03-24 23:34:17 -0600

    chore: :truck: Add the matches and groups routes to the prncipal router

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import players from './players.routes.js';
 import teams from './teams.routes.js';
+import groups from './groups.routes.js';
+import matches from './matches.routes.js';
 
 const router = Router();
 
 router.use('/players', players);
 router.use('/teams', teams);
+router.use('/groups', groups);
+router.use('/matches', matches);
 
 export default router;
+
+// TODO: Add a way to get the teams with the players
+// TODO: Add a way to get the groups with the teams
+// TODO: Add a way to get the matches with the teams
